5ff78fe5b32d600ec3b95585d9c8b8bb2dfcead5
bapuliz <87442802+bapuliz@users.noreply.github.com>
2024-06-10 23:21:43 -0700

    added class

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std::string_literals;
 
-int hash(const std::string& t) {
+#define HASHTABLE_BUCKET_SIZE 8
+#define HASHTABLE_LOAD_FACTOR (2.0/3.0)
+
+class HashTable {
+    private:
+        size_t n_buckets;
+        double load_factor;
+        HashTable() = delete;
+        HashTable(HashTable&) = delete;
+    public:
+        std::vector<int> HashTable::hash(const std::vector<std::string>&);
+        int HashTable::hash(const std::string&);
+        // HashTable(std::initializer_list<std::string> entries)
+        HashTable(std::vector<std::string>& entries)
+                     : n_buckets(HASHTABLE_BUCKET_SIZE),
+                       load_factor(HASHTABLE_LOAD_FACTOR)
+                {
+                    if (size(entries) >= static_cast<size_t>(load_factor * n_buckets)) {
+                        // To find the power of HASHTABLE_GROWTH_FACTOR:
+                        // ceil(2^x * HASHTABLE_BUCKET_SIZE * HASHTABLE_LOAD_FACTOR) > size(entries)
+                        // simplified:
+                        // size_t power = ceil((log2(size(entries) / (n_buckets) * (load_factor))));
+                        // n_buckets *= pow(2, power);
+                    }
+                }
+
+};
+int HashTable::hash(const std::string& t) {
     int hash = 0;
     for (std::string::const_iterator it = t.begin(); it != t.end(); ++it) {
         hash += *it;
@@ -10,7 +38,7 @@ int hash(const std::string& t) {
     std::cout << hash << std::endl;
     return hash;
 }
-std::vector<int> hash(const std::vector<std::string>& v){
+std::vector<int> HashTable::hash(const std::vector<std::string>& v){
     std::vector<int> hashes(v.size());
     for (std::vector<std::string>::const_reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
         hashes.push_back(hash(*it));
@@ -19,7 +47,9 @@ std::vector<int> hash(const std::vector<std::string>& v){
 }
 
 int main() {
-    std::vector a = {"hi"s, "abc"s, "aa"s, "qs"s, "pl"s};
-    hash(a);
+    std::vector<std::string> t(170, "hello");
+    // HashTable h ({"hi"s, "abc"s, "aa"s, "qs"s, "pl"s});
+    // HashTable h(t);
+
     return 0;
 }
